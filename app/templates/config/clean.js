yeoman/generator-webapp_DEPRECATED

separates task configs to files

app/templates/grunt/clean.js
@@ -1,0 +1,15 @@
+'use strict';
+// Empties folders to start fresh
+module.exports = {
+    dist: {
+        files: [{
+            dot: true,
+            src: [
+                '.tmp',
+                '<%%= yeoman.dist %>/*',
+                '!<%%= yeoman.dist %>/.git*'
+            ]
+        }]
+    },
+    server: '.tmp'
+};

app/templates/grunt/coffee.js
@@ -1,0 +1,22 @@
+'use strict';
+// Compiles CoffeeScript to JavaScript
+module.exports = {
+    dist: {
+        files: [{
+            expand: true,
+            cwd: '<%%= yeoman.app %>/scripts',
+            src: '{,*/}*.{coffee,litcoffee,coffee.md}',
+            dest: '.tmp/scripts',
+            ext: '.js'
+        }]
+    },
+    test: {
+        files: [{
+            expand: true,
+            cwd: 'test/spec',
+            src: '{,*/}*.{coffee,litcoffee,coffee.md}',
+            dest: '.tmp/spec',
+            ext: '.js'
+        }]
+    }
+};

app/templates/grunt/compass.js
@@ -1,0 +1,28 @@
+'use strict';
+// Compiles Sass to CSS and generates necessary files if requested
+module.exports = {
+    options: {
+        sassDir: '<%%= yeoman.app %>/styles',
+        cssDir: '.tmp/styles',
+        generatedImagesDir: '.tmp/images/generated',
+        imagesDir: '<%%= yeoman.app %>/images',
+        javascriptsDir: '<%%= yeoman.app %>/scripts',
+        fontsDir: '<%%= yeoman.app %>/styles/fonts',
+        importPath: '<%%= yeoman.app %>/bower_components',
+        httpImagesPath: '/images',
+        httpGeneratedImagesPath: '/images/generated',
+        httpFontsPath: '/styles/fonts',
+        relativeAssets: false,
+        assetCacheBuster: false
+    },
+    dist: {
+        options: {
+            generatedImagesDir: '<%%= yeoman.dist %>/images/generated'
+        }
+    },
+    server: {
+        options: {
+            debugInfo: true
+        }
+    }
+};

app/templates/grunt/htmlmin.js
@@ -1,0 +1,22 @@
+'use strict';
+
+module.exports = {
+    dist: {
+        options: {
+            collapseBooleanAttributes: true,
+            collapseWhitespace: true,
+            removeAttributeQuotes: true,
+            removeCommentsFromCDATA: true,
+            removeEmptyAttributes: true,
+            removeOptionalTags: true,
+            removeRedundantAttributes: true,
+            useShortDoctype: true
+        },
+        files: [{
+            expand: true,
+            cwd: '<%%= yeoman.dist %>',
+            src: '{,*/}*.html',
+            dest: '<%%= yeoman.dist %>'
+        }]
+    }
+};

app/templates/grunt/modernizr.js
@@ -1,0 +1,13 @@
+'use strict';
+// Generates a custom Modernizr build that includes only the tests you
+// reference in your app
+module.exports = {
+    devFile: '<%%= yeoman.app %>/bower_components/modernizr/modernizr.js',
+    outputFile: '<%%= yeoman.dist %>/bower_components/modernizr/modernizr.js',
+    files: [
+        '<%%= yeoman.dist %>/scripts/{,*/}*.js',
+        '<%%= yeoman.dist %>/styles/{,*/}*.css',
+        '!<%%= yeoman.dist %>/scripts/vendor/*'
+    ],
+    uglify: true
+};

app/templates/grunt/rev.js
@@ -1,0 +1,14 @@
+'use strict';
+// Renames files for browser caching purposes
+module.exports = {
+    dist: {
+        files: {
+            src: [
+                '<%%= yeoman.dist %>/scripts/{,*/}*.js',
+                '<%%= yeoman.dist %>/styles/{,*/}*.css',
+                '<%%= yeoman.dist %>/images/{,*/}*.{gif,jpeg,jpg,png,webp}',
+                '<%%= yeoman.dist %>/styles/fonts/{,*/}*.*'
+            ]
+        }
+    }
+};

app/templates/grunt/useminPrepare.js
@@ -1,0 +1,10 @@
+'use strict';
+// Reads HTML for usemin blocks to enable smart builds that automatically
+// concat, minify and revision files. Creates configurations in memory so
+// additional tasks can operate on them
+module.exports = {
+    options: {
+        dest: '<%%= yeoman.dist %>'
+    },
+    html: '<%%= yeoman.app %>/index.html'
+};

creynders/generator-webapp-impro

app/templates/config/clean.js
@@ -1,0 +1,15 @@
+'use strict';
+// Empties folders to start fresh
+module.exports = {
+    dist: {
+        files: [{
+            dot: true,
+            src: [
+                '.tmp',
+                '<%%= settings.dist %>/*',
+                '!<%%= settings.dist %>/.git*'
+            ]
+        }]
+    },
+    server: '.tmp'
+};

app/templates/config/coffee.js
@@ -1,0 +1,22 @@
+'use strict';
+// Compiles CoffeeScript to JavaScript
+module.exports = {
+    dist: {
+        files: [{
+            expand: true,
+            cwd: '<%%= settings.app %>/scripts',
+            src: '{,*/}*.{coffee,litcoffee,coffee.md}',
+            dest: '.tmp/scripts',
+            ext: '.js'
+        }]
+    },
+    test: {
+        files: [{
+            expand: true,
+            cwd: 'test/spec',
+            src: '{,*/}*.{coffee,litcoffee,coffee.md}',
+            dest: '.tmp/spec',
+            ext: '.js'
+        }]
+    }
+};

app/templates/config/compass.js
@@ -1,0 +1,28 @@
+'use strict';
+// Compiles Sass to CSS and generates necessary files if requested
+module.exports = {
+    options: {
+        sassDir: '<%%= settings.app %>/styles',
+        cssDir: '.tmp/styles',
+        generatedImagesDir: '.tmp/images/generated',
+        imagesDir: '<%%= settings.app %>/images',
+        javascriptsDir: '<%%= settings.app %>/scripts',
+        fontsDir: '<%%= settings.app %>/styles/fonts',
+        importPath: '<%%= settings.app %>/bower_components',
+        httpImagesPath: '/images',
+        httpGeneratedImagesPath: '/images/generated',
+        httpFontsPath: '/styles/fonts',
+        relativeAssets: false,
+        assetCacheBuster: false
+    },
+    dist: {
+        options: {
+            generatedImagesDir: '<%%= settings.dist %>/images/generated'
+        }
+    },
+    server: {
+        options: {
+            debugInfo: true
+        }
+    }
+};

app/templates/config/htmlmin.js
@@ -1,0 +1,22 @@
+'use strict';
+
+module.exports = {
+    dist: {
+        options: {
+            collapseBooleanAttributes: true,
+            collapseWhitespace: true,
+            removeAttributeQuotes: true,
+            removeCommentsFromCDATA: true,
+            removeEmptyAttributes: true,
+            removeOptionalTags: true,
+            removeRedundantAttributes: true,
+            useShortDoctype: true
+        },
+        files: [{
+            expand: true,
+            cwd: '<%%= settings.dist %>',
+            src: '{,*/}*.html',
+            dest: '<%%= settings.dist %>'
+        }]
+    }
+};

app/templates/config/modernizr.js
@@ -1,0 +1,13 @@
+'use strict';
+// Generates a custom Modernizr build that includes only the tests you
+// reference in your app
+module.exports = {
+    devFile: '<%%= settings.app %>/bower_components/modernizr/modernizr.js',
+    outputFile: '<%%= yeoman.dist %>/scripts/vendor/modernizr.js',
+    files: [
+        '<%%= settings.dist %>/scripts/{,*/}*.js',
+        '<%%= settings.dist %>/styles/{,*/}*.css',
+        '!<%%= settings.dist %>/scripts/vendor/*'
+    ],
+    uglify: true
+};

app/templates/config/rev.js
@@ -1,0 +1,14 @@
+'use strict';
+// Renames files for browser caching purposes
+module.exports = {
+    dist: {
+        files: {
+            src: [
+                '<%%= settings.dist %>/scripts/{,*/}*.js',
+                '<%%= settings.dist %>/styles/{,*/}*.css',
+                '<%%= settings.dist %>/images/{,*/}*.{gif,jpeg,jpg,png,webp}',
+                '<%%= settings.dist %>/styles/fonts/{,*/}*.*'
+            ]
+        }
+    }
+};

app/templates/config/useminPrepare.js
@@ -1,0 +1,10 @@
+'use strict';
+// Reads HTML for usemin blocks to enable smart builds that automatically
+// concat, minify and revision files. Creates configurations in memory so
+// additional tasks can operate on them
+module.exports = {
+    options: {
+        dest: '<%%= settings.dist %>'
+    },
+    html: '<%%= settings.app %>/index.html'
+};